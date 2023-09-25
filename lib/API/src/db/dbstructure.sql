a41806dba0d52b331c758685b11ceecad25c0381
JULIAN ROJAS <juliandra140201@gmail.com>
2023-09-25 16:24:25 -0500

    Structure of db

--- lib/API/src/db/dbstructure.sql
+++ lib/API/src/db/dbstructure.sql
@@ -2,12 +2,14 @@ create table empresas(
 	idempresa INT PRIMARY KEY,
 	nombreempresa VARCHAR(255),
 	imagenempresa VARCHAR(255)
-)
+);
+CREATE SEQUENCE empresas_idempresa_seq START 1;
+ALTER TABLE empresas ALTER COLUMN idempresa SET DEFAULT nextval('empresas_idempresa_seq');
 CREATE TABLE rutas(
     idruta INT PRIMARY KEY,
     nombreruta VARCHAR(255),
     idempresa INT,
     FOREIGN KEY (idempresa) REFERENCES empresas(idempresa)
 );
-select * from empresas;
+drop table empresas;
 select * from rutas;
